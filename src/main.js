a9b0484ff462e07393015a08fdea3cb137ea5032
jonah <jonahkf@gmail.com>
2025-02-23 18:08:34 +0900

    Added: Scroll function, demo circle to rotate

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-// import "./style.css";
+import "./style.css";
 
 console.log("main.js loaded");
+
+const screenController = () => {
+  window.onscroll = () => {
+    rotateCircle();
+  };
+
+  function rotateCircle() {
+    let circle = document.querySelector(".circle");
+    circle.style.transform = `rotate(${window.scrollY / 4}deg)`;
+  }
+};
+
+screenController();
